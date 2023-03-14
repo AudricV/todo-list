fc7bde1452ef56f296213c839610809bb8df3de2
AudricV <74829229+AudricV@users.noreply.github.com>
2023-03-14 19:26:48 +0100

    Display TODO items in MainContent
    Also fix ListDataAccess import.

--- src/main_content/MainContent.js
+++ src/main_content/MainContent.js
@@ -1,17 +1,47 @@
+import "./MainContent.css";
 import React from "react";
-import DataAccess from "../data_access/ListDataAccess";
+import ListDataAccess from "../data_access/ListDataAccess";
+import ListItem from "../list_item/ListItem";
 import PropTypes from "prop-types";
 
 class MainContent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasksList: this.props.dataAccess.getTasksList()
+        };
+    }
+
     render() {
         return (
-            <main></main>
+            <main>
+                <ul className="list_items">
+                    {
+                        this.state.tasksList.map((listItemData, index) =>
+                            <ListItem
+                                key={index}
+                                listItem={listItemData}
+                                onCheckedChangedEvent={
+                                    newCheckedListItemValue => {
+                                        listItemData.isChecked = newCheckedListItemValue;
+                                        this.props.dataAccess.setTasksList(this.state.tasksList);
+                                    }
+                                }
+                                onTitleChangedEvent={
+                                    newTitleListItemValue => {
+                                        listItemData.title = newTitleListItemValue;
+                                        this.props.dataAccess.setTasksList(this.state.tasksList);
+                                    }
+                                } />)
+                    }
+                </ul>
+            </main>
         );
     }
 }
 
 MainContent.propTypes = {
-    dataAccess: PropTypes.instanceOf(DataAccess)
+    dataAccess: PropTypes.instanceOf(ListDataAccess)
 };
 
 export default MainContent;
