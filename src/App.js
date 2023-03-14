a7cd1e14a72e6f128bdd16ee90f349c75a56f87b
AudricV <74829229+AudricV@users.noreply.github.com>
2023-03-14 13:00:33 +0100

    Replace default App component by the initial one of the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import MainContent from "./main_content/MainContent";
+import LocalStorageDataAccess from "./data_access/LocalStorageListDataAccess";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataAccess: new LocalStorageDataAccess()
+        };
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header dataAccess={this.state.dataAccess}></Header>
+                <MainContent dataAccess={this.state.dataAccess}></MainContent>
+                <Footer dataAccess={this.state.dataAccess}></Footer>
+            </div>
+        );
+    }
 }
 
 export default App;
